2497f51f2ce7f8701198181e902421fdabef1af2
bebeto9009 <adr.pawlak@gmail.com>
2019-12-27 22:19:51 +0100

    added background image

---
+++ src/components/Claim.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ClaimWrapper">
+    <h1 class="claim">spacer</h1>
+    <p class="subclaim">Search what do you want from space! <br> Have fun!</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Claim',
+};
+</script>
+<style lang="scss" scoped>
+  .ClaimWrapper {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .claim {
+    font-size: 50px;
+    margin: 0;
+    text-transform: uppercase;
+    color: #fff;
+  }
+
+  .subclaim {
+    margin: 20px 0 0 0;
+    font-size: 13px;
+    color: #fff;
+  }
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,30 +1,24 @@
 <template>
   <div class="wrapper">
-    <div class="search">
-      <label for="search">Search</label>
-      <input
-          id="search"
-          name="search"
-          v-model="searchValue"
-          @input="handleInput"
-      />
-      <ul>
-        <li v-for="item in results" :key="item.data[0].nasa_id">
-          <p>{{ item.data[0].description }} </p>
-        </li>
-      </ul>
-    </div>
+    <Claim />
+    <SearchInput />
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import debounce from 'lodash.debounce';
+import Claim from '@/components/Claim.vue';
+import SearchInput from '@/components/SearchInput.vue';
 
 const API = 'http://images-api.nasa.gov/search';
 
 export default {
   name: 'Search',
+  components: {
+    Claim,
+    SearchInput,
+  },
   data() {
     return {
       searchValue: '',
@@ -32,6 +26,7 @@ export default {
     };
   },
   methods: {
+    // eslint-disable-next-line
     handleInput: debounce(function () {
       axios.get(`${API}?q=${this.searchValue}&media_type=image`)
         .then((response) => {
@@ -47,26 +42,16 @@ export default {
 <style lang="scss" scoped>
   .wrapper {
     display: flex;
+    height: 100vh;
     flex-direction: column;
     align-items: center;
     margin: 0;
     padding: 30px;
+    justify-content: center;
     width: 100%;
-  }
-
-  .search {
-    display: flex;
-    flex-direction: column;
-    width: 300px;
-
-    label {
-      font-family: Montserrat, sans-serif;
-    }
-
-    input {
-      height: 30px;
-      border: 0;
-      border-bottom: 1px solid black;
-    }
+    background-image: url('../assets/astronomy-atmosphere-earth-exploration-220201.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 40% 50%;
   }
 </style>
